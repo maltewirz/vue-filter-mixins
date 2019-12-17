fa84eafc36cf267e073dc82d1be60ffe401eb237
Malte Wirz <mail@maltewirz.com>
2019-12-17 09:23:02 +0100

    mixin added

--- src/List.vue
+++ src/List.vue
@@ -3,38 +3,20 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
-                {{ text | toUpperCase | to-lowercase}}
                 <input v-model="filterText">
                 <ul>
                     <li v-for="(fruit, index) in filteredFruits" :key="index"> {{ fruit }}</li>
                 </ul>
-                <hr>
-                <List/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import List from './List.vue';
-    import { fruitMixin } from './fruitMixin';
 
+    import { fruitMixin } from './fruitMixin';
     export default {
-        mixins: [fruitMixin],
-        data() {
-            return {
-                text: 'Hello there!',
-                
-            }
-        },
-        filters: {
-            toUpperCase(value) {
-                return value.toUpperCase();
-            }
-        },
-        components: {
-            List
-        }
+        mixins: [fruitMixin]
     }
 </script>
 
